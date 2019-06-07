b21236950ebfab90284006838cafe890aa9ab102
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-06-08 02:15:33 +0300

    'Creating-Pagination-Components'

--- src/containers/News.js
+++ src/containers/News.js
@@ -4,11 +4,13 @@ import Title from '../components/title/title';
 import NewsPost from '../components/news_post/news_post';
 import Input from '../components/input/input';
 import Select from '../components/select/select';
+import Pagination from '../components/pagination/pagination';
 
 const BASE_PATH = 'https://hn.algolia.com/api/v1';
 const SEARCH_PATH = '/search';
 const SEARCH_PARAM = 'query=';
 const PAGE_HITS = 'hitsPerPage=';
+const PAGE_PARAM = 'page=';
 
 const HITS = [
 	{
@@ -30,16 +32,17 @@ class News extends React.Component{
 	state = {
 		searchQuery: '',
 		result: {},
-		hitsPerPage: 20
+		hitsPerPage: 20,
+		page: 0
 	}
 
 	componentDidMount(){
-		const {searchQuery, hitsPerPage} = this.state;
-		this.fetchData(searchQuery, hitsPerPage);
+		const {searchQuery, hitsPerPage, page} = this.state;
+		this.fetchData(searchQuery, hitsPerPage, page);
 	}
 
-	fetchData = (searchQuery ,hitsPerPage) => {
-		fetch(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}`)
+	fetchData = (searchQuery ,hitsPerPage, page) => {
+		fetch(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAM}${searchQuery}&${PAGE_HITS}${hitsPerPage}&${PAGE_PARAM}${page}`)
 			.then(res => res.json())
 			.then(result => this.setNews(result))
 			.catch(error => error);
@@ -48,9 +51,12 @@ class News extends React.Component{
 	getSearch = ({key}) => {
 		if (key === 'Enter'){
 			const {searchQuery, hitsPerPage} = this.state;
-			this.fetchData(searchQuery, hitsPerPage);
+			this.setState({
+				page: 0
+			})
+			this.fetchData(searchQuery, hitsPerPage, 0);
 		}
-	}
+	};
 
 	handleInputChange = ({target: {value}}) => {
 		this.setState ({
@@ -67,19 +73,52 @@ class News extends React.Component{
 
 		this.setState({
 			hitsPerPage: +value,
+			page: 0
 		}, () => {
-			this.fetchData(searchQuery, this.state.hitsPerPage);
+			this.fetchData(searchQuery, this.state.hitsPerPage, 0);
 		})
-	}
+	};
+
+	handlePageChange = ({ target }) => {
+		const btnType = target.getAttribute('data-name');
+		let { page } = this.state;
+
+		if(!isNaN(btnType)) {
+			this.updatePage(+btnType);
+		} else {
+			switch (btnType) {
+				case 'next':
+					this.updatePage(page + 1);
+					break;
+				case 'prev':
+					this.updatePage(page - 1);
+					break;
+			}
+		}
+	};
+
+	updatePage = (number) => {
+		const { searchQuery, hitsPerPage } = this.state;
+		this.setState({
+			page: number,
+		}, () => {
+			this.fetchData(searchQuery, hitsPerPage, number);
+		})
+	};
 
 	render(){
 		const {searchQuery, result, hitsPerPage} = this.state;
-		const {hits = [] } = result;
+		const {hits = [] , page , nbPages} = result;
 
 		return(
 			<div className="wrapper">
 				<Title title="Hacker News" />
 				<Select handleChange={this.handleHitsChange} options={HITS} value={hitsPerPage} />
+				<Pagination
+					onClick={this.handlePageChange}
+					page={page}
+					lastPage={nbPages}
+				/>
 				<Input onKeyPress={this.getSearch} onChange={this.handleInputChange} value={searchQuery} />
 				<ul className="newsList">
 					{hits.map(({ author, created_at, num_comments, objectID, title, points, url }) =>
