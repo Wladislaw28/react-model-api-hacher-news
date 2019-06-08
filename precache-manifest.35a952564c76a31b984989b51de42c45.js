255f66bb51ad7733858caed9bc1db7925a513147
Владислав <40837008+Wladislaw28@users.noreply.github.com>
2019-06-08 15:12:28 +0300

    Updates

---
+++ precache-manifest.35a952564c76a31b984989b51de42c45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82aaccf94d875b25e736e642995fc49",
+    "url": "/react-model-api-hacher-news/index.html"
+  },
+  {
+    "revision": "aaf5191e99ef9aec9076",
+    "url": "/react-model-api-hacher-news/static/css/main.79796650.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-model-api-hacher-news/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "aaf5191e99ef9aec9076",
+    "url": "/react-model-api-hacher-news/static/js/main.10b3139d.chunk.js"
+  },
+  {
+    "revision": "4b1d2806f6799dfd9322",
+    "url": "/react-model-api-hacher-news/static/js/runtime~main.b483da17.js"
+  }
+]);
